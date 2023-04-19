567b7f70be2be20d073a4494b56b2f76bbe1e9f6
jifengzhiyu <2716105592@qq.com>
2023-04-19 22:43:35 +0800

    Initial commit

---
+++ src/test/java/com/gcdic/GcdicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcdic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcdicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
